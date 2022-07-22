fec69141cccc906a329f19bff50d47da7b17b5be
Markus Hädrich <markus.haedrich@mail.de>
2022-07-22 17:09:42 +0200

    last commit before holidays

---
+++ lib/TestsRTKBaseStation/TestsRTKBaseStation.h
@@ -0,0 +1,12 @@
+#ifndef TESTS_RTK_BASE_STATION_H
+#define TESTS_RTK_BASE_STATION_H
+
+#include <AUnit.h>
+#include <RTKBaseStation.h>
+
+// test(saveLocation) {
+
+// }
+
+
+#endif /*** TESTS_RTK_BASE_STATION_H ***/
